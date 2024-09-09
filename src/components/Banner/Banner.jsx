7f90c6d7241d7c11289ecff81054a595b8ce8f20
Yasaman Abedi <Yasaman471@gmail.com>
2024-09-09 17:22:04 +0330

    Created Banner Component and added  its Image and Style

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+const Banner = () =>{
+    return (
+        <div className="banner">
+            <img src="public/Image/1.jpg" alt="BannerPic" className="banner-img" />
+            <div className="banner-content">
+                <h1>Sip, Savor, Repeat</h1>
+                <p>Indulge in expertly crafted coffee, cozy atmosphere, and warm hospitality at our charming coffee shop, where every sip feels like home.</p>
+                <button>Started</button>
+            </div>
+        </div>
+    );
+}
+
+export default Banner
